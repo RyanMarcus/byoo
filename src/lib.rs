ab759f0687dabf113825cd795290a0ba308ce502
Ryan Marcus <ryan@rmarcus.info>
2018-10-09 15:58:51 -0400

    sort operator

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ extern crate csv;
 extern crate tempfile;
 extern crate base64;
 extern crate binary_heap_plus;
+extern crate rand;
 
 mod data;
 
---
+++ src/operator/sort.rs
@@ -0,0 +1,196 @@
+use operator_buffer::{OperatorReadBuffer, OperatorWriteBuffer, PeekableOperatorReadBuffer};
+use spillable_store::WritableSpillableStore;
+use data::{Data, DataType};
+use std::mem;
+use std::cmp::Ordering;
+use binary_heap_plus::*;
+
+pub struct Sort {
+    input: OperatorReadBuffer,
+    output: OperatorWriteBuffer,
+    by_cols: Vec<usize>,
+    buf: Vec<Data>,
+    buf_size: usize
+}
+
+impl Sort {
+    fn new(input: OperatorReadBuffer, output: OperatorWriteBuffer,
+           by_cols: Vec<usize>, buf_size: usize) -> Sort {
+        return Sort {
+            input, output, by_cols, buf_size,
+            buf: Vec::with_capacity(buf_size)
+        };
+    }
+
+    fn sort_and_dump_buf<F>(buf: Vec<Data>,
+                            types: &[DataType],
+                            sort_fn: F) -> WritableSpillableStore
+    where F: Fn(&&[Data], &&[Data]) -> Ordering {
+
+        // sort the buffer
+        let mut by_rows: Vec<&[Data]> = buf.as_slice()
+            .chunks(types.len())
+            .collect();
+
+        by_rows.sort_unstable_by(sort_fn);
+
+        let mut store = WritableSpillableStore::new(
+            1024, types.to_vec());
+
+        for row in by_rows {
+            store.push_row(row);
+        }
+
+        return store;
+    }
+    
+    fn start(mut self) {
+        let types = self.input.types().to_vec();
+        let by_cols = self.by_cols.clone();
+        let mut chunks = Vec::new();
+
+        let sort_fn = |el1: &&[Data], el2: &&[Data]| {
+            for &col_idx in by_cols.iter() {
+                match el1[col_idx].partial_cmp(&el2[col_idx]).unwrap() {
+                    Ordering::Greater => { return Ordering::Greater; },
+                    Ordering::Less =>  { return Ordering::Less; }
+                    _ => {}
+                };
+            }
+            return Ordering::Equal;
+        };
+        
+        iterate_buffer!(self.input, row, {
+            self.buf.extend_from_slice(row);
+
+            if self.buf.len() >= self.buf_size {
+                // time to dump this chunk to disk.
+                let mut loc_buf = Vec::with_capacity(self.buf_size);
+                mem::swap(&mut self.buf, &mut loc_buf);
+
+                let r = Sort::sort_and_dump_buf(loc_buf, &types, sort_fn);
+                chunks.push(r);
+            }
+        });
+
+        // dump the remaining rows in the buffer, if any.
+        if !self.buf.is_empty() {
+            let mut loc_buf = Vec::with_capacity(0);
+            mem::swap(&mut self.buf, &mut loc_buf);
+
+            let r = Sort::sort_and_dump_buf(loc_buf, &types, sort_fn);
+            chunks.push(r);
+        }
+
+        // next, we have to merge the sorted fragments.
+        let readers: Vec<PeekableOperatorReadBuffer> = chunks.iter_mut()
+            .map(|r| r.read())
+            .map(|(_, r)| PeekableOperatorReadBuffer::new(r))
+            .collect();
+
+        let mut bheap = BinaryHeap::new_by(
+            |h1: &PeekableOperatorReadBuffer,
+             h2: &PeekableOperatorReadBuffer| -> Ordering {
+                
+                 let r1 = h1.peek();
+                 let r2 = h2.peek();
+
+                 // reverse here because the heap is a max heap
+                 return sort_fn(&r1.unwrap().as_slice(),
+                                &r2.unwrap().as_slice()).reverse();
+            });
+        
+        for r in readers {
+            if let None = r.peek() {
+                continue;
+            }
+            bheap.push(r);
+        }
+
+        while !bheap.is_empty() {
+            let mut next_reader = bheap.pop().unwrap();
+            let next_row = next_reader.pop().unwrap();
+
+            // write the row to the output, add the reader
+            // back into the heap.
+            self.output.write(next_row);
+
+            if let Some(_) = next_reader.peek() {
+                bheap.push(next_reader);
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use operator::Sort;
+    use operator_buffer::{OperatorReadBuffer, OperatorWriteBuffer, make_buffer_pair};
+    use data::{Data, DataType};
+    use rand::prelude::*;
+    use std::thread;
+    
+    #[test]
+    fn sorts_single_col() {
+        let (r, mut w) = make_buffer_pair(5, 10, vec![DataType::INTEGER]);
+        let (mut r2, w2) = make_buffer_pair(5, 10, vec![DataType::INTEGER]);
+
+        w.write(vec![Data::Integer(-50)]);
+        w.write(vec![Data::Integer(-90)]);
+        w.write(vec![Data::Integer(40)]);
+        w.write(vec![Data::Integer(0)]);
+        w.write(vec![Data::Integer(19)]);
+        drop(w);
+
+        let s = Sort::new(r, w2, vec![0], 100);
+        s.start();
+
+        let mut res = Vec::new();
+        iterate_buffer!(r2, row, {
+            if let Data::Integer(i) = row[0] {
+                res.push(i);
+            } else {
+                panic!("wrong datatype");
+            }
+        });
+
+        assert_eq!(res, vec![-90, -50, 0, 19, 40]);
+    }
+
+    #[test]
+    fn sorts_spill() {
+        let (r, mut w) = make_buffer_pair(5, 10, vec![DataType::INTEGER]);
+        let (mut r2, w2) = make_buffer_pair(5, 10, vec![DataType::INTEGER]);
+
+        let mut data = Vec::new();
+        let mut byoo_data = Vec::new();
+        for _ in 0..10005 {
+            let r = random::<i64>();
+            data.push(r);
+            byoo_data.push(Data::Integer(r));
+        }
+        data.sort();
+
+        thread::spawn(move || {
+            for d in byoo_data {
+                w.write(vec![d]);
+            }
+        });
+
+        thread::spawn(move || {
+            let s = Sort::new(r, w2, vec![0], 100);
+            s.start();
+        });
+
+        let mut res = Vec::new();
+        iterate_buffer!(r2, row, {
+            if let Data::Integer(i) = row[0] {
+                res.push(i);
+            } else {
+                panic!("wrong datatype");
+            }
+        });
+
+        assert_eq!(res, data);
+    }
+}
